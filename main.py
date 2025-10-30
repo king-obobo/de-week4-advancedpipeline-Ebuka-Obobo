e3748f9863dedb480a742c2d680d9f9b48bc770b
king-obobo <oboboebuka1@gmail.com>
2025-10-30 23:18:24 +0100

    Completed the Pipeline and Added an exporter class to handle exporting

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-from pipeline.api_client import APIClient
-from pprint import pprint
-from pipeline.data_enricher import DataEnricher
-from pipeline.data_analyzer import Analyzer
+from pipeline.pipeline import Pipeline
 
 
 if __name__ == "__main__":
-    api_client = APIClient()
-    all_products = api_client.get_all_products()
-    # pprint(all_products[:2])
-    all_users = api_client.get_all_users()
-    # pprint(all_users[:2])
-    
-    enricher = DataEnricher(all_products, all_users)
-    df = enricher.enrich_data()
-    # pprint(df.head())
-    
-    df_analyzer = Analyzer(df) 
-    analysis = df_analyzer.perform_analysis()
-    
-    pprint(analysis)
-    
-    #Run analysis
-    pass
+    try:
+        my_pipeline = Pipeline()
+        my_pipeline.run()
+    except Exception as e:
+        print(f"An error occured: {e}")
